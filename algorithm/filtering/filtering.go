e846321cec4707a50bbec53c5f1db1ed53449624
keinuma <nununu.mono@gmail.com>
2025-02-24 16:47:17 +0900

    2の倍数でフィルタリングする関数追加

--- algorithm/filtering/filtering.go
+++ algorithm/filtering/filtering.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	src := []int{1, 2, 3, 4, 5}
+	dst := src[:0]
+
+	fmt.Println("original: ", src)
+	fmt.Println("output: ", dst)
+
+	for _, v := range src {
+		if even(v) {
+			dst = append(dst, v)
+		}
+	}
+
+	fmt.Println("---------")
+	fmt.Println("original: ", src)
+	fmt.Println("output: ", dst)
+
+	for i := len(dst); i < len(src); i++ {
+		src[i] = 0
+	}
+
+	fmt.Println("---------")
+	fmt.Println("original: ", src)
+	fmt.Println("output: ", dst)
+}
+
+func even(n int) bool {
+	return n%2 == 0
+}
