cbf68c932f7fdd0bf2a99eb2fd5c1f4332c02720
keinuma <nununu.mono@gmail.com>
2025-02-25 21:49:39 +0900

    クイックソートを実装

---
+++ algorithm/quick_sort/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	nums := random_nums(1000)
+	s := time.Now()
+
+	quick_sort(nums)
+
+	elapsed := time.Since(s)
+	fmt.Println("processimg time: ", elapsed)
+}
+
+func quick_sort(nums []int) []int {
+	if len(nums) <= 1 {
+		return nums
+	}
+	bi := rand.Intn(len(nums))
+	b := nums[bi]
+
+	left, right := make([]int, 0), make([]int, 0)
+	for _, v := range nums {
+		if b > v {
+			left = append(left, v)
+		} else if b < v {
+			right = append(right, v)
+		}
+	}
+	return append(append(quick_sort(left), b), quick_sort(right)...)
+}
+
+func random_nums(n int) []int {
+	output := make([]int, 0)
+	for i := 0; i < n; i++ {
+		output = append(output, rand.Intn(1000))
+	}
+	return output
+}
